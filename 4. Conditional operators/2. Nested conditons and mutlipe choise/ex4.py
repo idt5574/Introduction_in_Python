c6bf3c0179b9e30dee0ad8dc7881c471cd6cc9d5
Isaykin Daniil Timofeevich <k0tk3y@ya.ru>
2025-01-26 20:48:38 +0300

    Day of the week

---
+++ 4. Conditional operators/2. Nested conditons and mutlipe choise/ex4.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+if n == 1:
+    print("понедельник")
+elif n == 2:
+    print("вторник")
+elif n == 3:
+    print("среда")
+elif n == 4:
+    print("четверг")
+elif n == 5:
+    print("пятница")
+elif n == 6:
+    print("суббота")
+elif n == 7:
+    print("воскресенье")
+    
