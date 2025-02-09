af43aebbdf70858ceecb9265bb98f7008e76e293
idt5574 <k0tk3y@ya.ru>
2025-02-09 12:59:39 +0300

    Put matrix in one line and reverse

---
+++ 5. Cycles, iterators and list generators/9. Nested cycles and nested list comprehension/ex1.py
@@ -0,0 +1,8 @@
+import sys
+
+# считывание списка из входного потока
+s = sys.stdin.readlines()
+lst_in = [list(map(int, x.strip().split())) for x in s]
+
+print(*[j for i in lst_in for j in i][::-1])
+
